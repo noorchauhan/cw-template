6e120afcc722f004dccdb727363865421fa827be
Orkun Külçe <orkunkl@users.noreply.github.com>
2021-11-22 10:33:03 +0300

    Clean boilerplate

--- examples/schema.rs
+++ examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use {{crate_name}}::msg::{CountResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
+use {{crate_name}}::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
 use {{crate_name}}::state::State;
 
 fn main() {
@@ -14,7 +14,7 @@ fn main() {
 
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(MigrateMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(State), &out_dir);
-    export_schema(&schema_for!(CountResponse), &out_dir);
 }
