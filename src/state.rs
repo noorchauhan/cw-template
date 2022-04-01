dbba260f539b644bf1864dd1fa54bdb3b2dbc68c
Pratyan Majumder <heypratyan@gmail.com>
2022-04-01 21:17:12 +0530

    Update state.rs

--- src/state.rs
+++ src/state.rs
@@ -11,3 +11,33 @@ pub struct State {
 }
 
 pub const STATE: Item<State> = Item::new("state");
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct StakerInfo {
+    pub bond-amount: Uint128,
+}
+
+pub fn store_staker_info(
+    storage: &mut dyn Storage,
+    owner: &CanonicalAddr,
+    staker_info: &StakerInfo,
+) -> StdResult<()> {
+    Bucket::new(storage, PREFIX_REWARD).save(owner.as_slice(), staker_info)
+}
+
+pub fn remove_staker_info(storage: &mut dyn Storage, owner: &CanonicalAddr) {
+    Bucket::<StakerInfo>::new(storage, PREFIX_REWARD).remove(owner.as_slice())
+}
+
+
+pub fn read_staker_info(storage: &dyn Storage, owner: &CanonicalAddr) -> StdResult<StakerInfo> {
+    match ReadonlyBucket::new(storage, PREFIX_REWARD).may_load(owner.as_slice())? {
+        Some(staker_info) => Ok(staker_info),
+        None => Ok(StakerInfo {
+            bond_amount: Uint128::zero(),
+           
+        }),
+    }
+}
+
+
